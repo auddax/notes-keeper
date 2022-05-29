113e9d9bef92e32d5ca79e2703f8d4e91c8db0ef
Kirill Konovalov <auddax@gmail.com>
2022-05-29 19:28:31 +0300

    feat: add custom styles for HeaderBar component (Sun, May 29, 2022 07:28:31 PM)

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,49 +1,39 @@
 import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNoteSticky } from '@fortawesome/free-regular-svg-icons'
+import { styled } from '@mui/material/styles';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { Menu } from '@mui/icons-material';
 
 const Header = styled(AppBar)`
   z-index: 1201;
   background: #fff;
   height: 70px;
+  color: #5f6368;
+  box-shadow: inset 0 -1px 0 0 #dadce0;
+  align-content: flex-end;
+`
+const Logo = styled(FontAwesomeIcon)`
+  font-size: 2rem;
+  margin: 0 0.9rem 0 0;
+`
+const Heading = styled(Typography)`
+  font-size: 1.25rem;
 `
 
 const HeaderBar = ({ open, handleDrawer }) => {
   return (
-    <Header position="fixed" open={ open }>
+    <Header open={ open }>
     <Toolbar>
       <IconButton
-        color="inherit"
-        aria-label="open drawer"
         onClick={ handleDrawer }
         edge="start"
-        sx={{
-          marginRight: 5,
-          ...(open && { display: 'none' }),
-        }}
+        sx={{ marginRight: '20px'}}
       >
-      <MenuIcon />
+      <Menu />
       </IconButton>
-      <Typography variant="h6" noWrap component="div">
-        Mini variant drawer
-      </Typography>
+      <Logo icon={faNoteSticky} />
+      <Heading>Notes Keeper</Heading>
     </Toolbar>
   </Header>
   );
